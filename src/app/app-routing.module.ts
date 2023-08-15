0f71e4185c7b587b4a866a99e134e766d7a4c802
joseduardo <joseduard0zeta@gmail.com>
2023-08-15 11:50:26 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { SignComponent } from './sign/sign.component';
+import { DetalheComponent } from './detalhe/detalhe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {path: 'produto', component: ProdutoComponent},
+  {path: 'sign', component: SignComponent},
+  {path: 'detalhe', component: DetalheComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
